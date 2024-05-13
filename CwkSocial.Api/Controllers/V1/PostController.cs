7bf5a7cebea2c70487066732985c27dad7c2de2a
Дмитрий Вылегжанин <vylegzhanin.d.a@mail.ru>
2024-05-14 01:34:58 +0500

    add api versioning

---
+++ CwkSocial.Api/Controllers/V1/PostController.cs
@@ -0,0 +1,20 @@
+﻿using Asp.Versioning;
+using Cwk.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CwkSocial.Api.Controllers.V1;
+
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+public class PostController: Controller
+{
+    [HttpGet]
+    [Route("{id:int}")]
+    public IActionResult GetById(int id)
+    {
+        var post = new Post { Id = id, Text = "Hello v1!"};
+
+        return Ok(post);
+    }
+}
--- CwkSocial.Api/Program.cs
+++ CwkSocial.Api/Program.cs
@@ -1,19 +1,38 @@
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using CwkSocial.Api.Options;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApiVersioning(config =>
+    {
+        config.DefaultApiVersion = new ApiVersion(1, 0);
+        config.AssumeDefaultVersionWhenUnspecified = true;
+        config.ReportApiVersions = true;
+        config.ApiVersionReader = new UrlSegmentApiVersionReader();
+    })
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+    });
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", $"CwkSocial API V{description.ApiVersion.ToString()}");
+        }
+    });
 }
 
 app.UseHttpsRedirection();
