425ebf590e89afe5a58376251b55bbed8c3c4aa8
Дмитрий Вылегжанин <vylegzhanin.d.a@mail.ru>
2024-05-18 02:34:57 +0500

    remove swaggerappregistrar

--- CwkSocial.Api/Registrars/MvcWebAppRegistrar.cs
+++ CwkSocial.Api/Registrars/MvcWebAppRegistrar.cs
@@ -1,11 +1,25 @@
-﻿namespace CwkSocial.Api.Registrars;
+﻿using Asp.Versioning.ApiExplorer;
+
+namespace CwkSocial.Api.Registrars;
 
 public class MvcWebAppRegistrar : IWebApplicationRegistrar
 {
     public void RegisterPipelineComponents(WebApplication app)
     {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", $"CwkSocial API V{description.ApiVersion.ToString()}");
+            }
+        });
+        
         app.UseHttpsRedirection();
+        
         app.UseAuthorization();
+        
         app.MapControllers();
     }
 }
